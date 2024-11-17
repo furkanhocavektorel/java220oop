838573f924cdf724a4541d7e19e979e4b32498a4
furkanhocavektorel <furkanhocavektorel@gmail.com>
2024-11-17 12:50:00 +0300

    first commit

---
+++ src/Runner.java
@@ -0,0 +1,17 @@
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+        InsanIsleri insanIsleri= new InsanIsleri();
+
+        insanIsleri.insanEkle();
+        insanIsleri.insanEkle();
+
+        insanIsleri.insanlariEkranaBastir();
+
+
+
+    }
+
+}
